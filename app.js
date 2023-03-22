1431eb704aeed84c37442f7413b29fecebfa3c82
Chris Wren <chris.wren@cef.co.uk>
2023-03-22 09:09:03 +0000

    Handlebars templates

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHandlebars = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHandlebars({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
